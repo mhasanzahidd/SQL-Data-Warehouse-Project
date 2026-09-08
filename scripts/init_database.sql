ce8f094d7d663b655893a41ed7ebe9e3d20cca69
Hasan Zahid <m.hasanzahid.06@gmail.com>
2026-09-08 16:20:57 +0500

    Create init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -9,7 +9,8 @@ Script Purpose:
 	
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script.
+    All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before 
+	running this script.
 */
 
 USE master;
