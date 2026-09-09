9499a2a9b9ca0e9156020f6890d710aabaf09a08
Hasan Zahid <m.hasanzahid.06@gmail.com>
2026-09-09 21:46:37 +0500

    Create ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,13 +13,13 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.crm_cust_info(
-	cst_id				      INT,
-	cst_key				      NVARCHAR(50),
-	cst_firstname		    NVARCHAR(50),
-	cst_lastname		    NVARCHAR(50),
-	cst_martial_status	NVARCHAR(50),
-	cst_gndr			      NVARCHAR(50),
-	cst_create_date		  DATE
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
+	cst_create_date		DATE
 );
 GO
 
@@ -29,10 +29,10 @@ GO
 
 CREATE TABLE bronze.crm_prd_info(
 	prd_id		  	INT,
-	prd_key			  NVARCHAR(50),
-	prd_nm			  NVARCHAR(50),
-	prd_cost		  INT,
-	prd_line		  NVARCHAR(50),
+	prd_key			NVARCHAR(50),
+	prd_nm			NVARCHAR(50),
+	prd_cost		INT,
+	prd_line		NVARCHAR(50),
 	prd_start_dt	DATE,
 	prd_end_dt		DATE
 );
@@ -49,9 +49,9 @@ CREATE TABLE bronze.crm_sales_details(
 	sls_order_dt	INT,
 	sls_ship_dt		INT,
 	sls_due_dt		INT,
-	sls_sales		  INT,
+	sls_sales		INT,
 	sls_quantity	INT,
-	sls_price		  INT
+	sls_price		INT
 );
 GO
 
@@ -81,9 +81,9 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2(
-	id			    NVARCHAR(50),
-	cat			    NVARCHAR(50),
-	subcat		  NVARCHAR(50),
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
+	subcat		NVARCHAR(50),
 	maintenance	NVARCHAR(50)
 );
 GO
